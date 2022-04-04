4c69d93078bb035bdd5c412c0369dfcb6a1ddf57
Liam Chivers <85449720+liamchivers23@users.noreply.github.com>
2022-04-04 15:15:27 +0100

    Added updated code

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-async function trivia() {
-  const data = await fetch("https://opentdb.com/api.php?amount=10&category=18");
-  const res = await data.json();
-  console.log(res.results);
-}
-trivia();
+const body = document.body;
+const container = document.querySelector(".container");
+const headerQuestion = document.querySelector(".question");
 
-//how to get data from api to display. ->
+fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
 
-// res.results.forEach((element) => {
-//   console.log(element.question);
-// });
+    let question = data.results[0].question;
+    let res = data.results;
+
+    // replace bad encoding with quotation marks.
+    for (let i = 0; i < question.length; i++) {
+      question = question.replace("&quot;", "'");
+      question = question.replace("&#039;", "'");
+    }
+
+    headerQuestion.innerText = question;
+
+    //push the correct answer to incorrect answer array for easier manipulation.
+    let answersOne = res[0].incorrect_answers;
+    answersOne.push(res[0].correct_answer);
+
+    //loop over the answers and display them in the container
+    for (let i = 0; i < 4; i++) {
+      let answers = answersOne[i];
+      container.innerHTML += `<div class="button"> ${answers} </div>`;
+    }
+
+    //check if button thats clicked is the correct answer.
+    const button = document.querySelectorAll(".button");
+    for (let i = 0; i < button.length; i++) {
+      button[i].addEventListener("click", () => {
+        let rightAnswer = res[0].correct_answer;
+
+        for (let j = 0; j < question.length; j++) {
+          rightAnswer = rightAnswer.replace("&quot;", "'");
+          rightAnswer = rightAnswer.replace("&#039;", "'");
+        }
+
+        button[i].innerText === rightAnswer
+          ? (button[i].style.backgroundColor = "green")
+          : (button[i].style.backgroundColor = "red");
+      });
+    }
+  });
